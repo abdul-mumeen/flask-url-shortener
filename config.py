df0588d170443e86c01ea005b207bd2e08d50286
andela-aolasode <abdulmumeen.olasode@andela.com>
2017-03-19 09:17:11 +0100

    Setup config.py file and add gitignore

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DEV_DATABASE_URL')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URL')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+config = {    'development': DevelopmentConfig,    'testing': TestingConfig,    'production': ProductionConfig,    'default': DevelopmentConfig}
