5c865ceaef3adbb97acc6d7795020816c564d9e6
andela-aolasode <abdulmumeen.olasode@andela.com>
2017-03-20 18:24:14 +0100

    [Feature] Modify model module to pass test cases

--- app/models.py
+++ app/models.py
@@ -1,4 +1,10 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+visits = db.Table('visits',
+                  db.Column('visitor_id', db.Integer, db.ForeignKey('visitors.visitor_id')),
+                  db.Column('url_id', db.Integer, db.ForeignKey('urls.url_id'))
+                  )
 
 
 class User(db.Model):
@@ -7,8 +13,19 @@ class User(db.Model):
     first_name = db.Column(db.String(30), index=True)
     last_name = db.Column(db.String(30), index=True)
     email = db.Column(db.String(64), unique=True)
-    password = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
     urls = db.relationship('Url', backref='user')
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User %r>' % self.first_name + " " + self.last_name
@@ -19,8 +36,8 @@ class Url(db.Model):
     url_id = db.Column(db.Integer, primary_key=True)
     short_url = db.Column(db.String(64), unique=True)
     long_url = db.Column(db.Text, index=True)
-    active = db.Column(db.Integer index=True)
-    deleted = db.Column(db.Integer)
+    active = db.Column(db.Integer, index=True, default=1)
+    deleted = db.Column(db.Integer, default=0)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
 
     def __repr__(self):
@@ -28,10 +45,13 @@ class Url(db.Model):
 
 
 class Visitor(db.Model):
-    __tablename__ = 'visitor'
-    vistor_id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'visitors'
+    visitor_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
     email = db.Column(db.String(64), index=True)
+    urls = db.relationship('Url', secondary=visits,
+                           backref=db.backref('visitors', lazy='dynamic'),
+                           lazy='dynamic')
 
     def __repr__(self):
         return '<Visitor %r>' % self.name
