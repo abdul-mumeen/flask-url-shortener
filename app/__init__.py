fe6eb1bee3b9444c176d165668e8b8a08c10b38f
andela-aolasode <abdulmumeen.olasode@andela.com>
2017-03-23 10:33:01 +0100

    [Feature] Setup migrate command and prevent circular dependencies

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,16 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
-from .main import main as main_blueprint
+from flask_migrate import Migrate
+
 
 bootstrap = Bootstrap()
 moment = Moment()
+migrate = Migrate()
 db = SQLAlchemy()
+from .main import main as main_blueprint
+from .api_1_0 import api as api_1_0_blueprint
+
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -17,6 +22,8 @@ def create_app(config_name):
     bootstrap.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    migrate.init_app(app, db)
     app.register_blueprint(main_blueprint)
+    app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
 
     return app
