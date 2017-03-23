c33e806f409e5a2a7846baab2cf5d6c84b256ff4
andela-aolasode <abdulmumeen.olasode@andela.com>
2017-03-23 10:38:47 +0100

    [Feature] Modify model for token authentication

--- app/models.py
+++ app/models.py
@@ -1,5 +1,10 @@
-from app import db
+from flask import current_app, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask_login import UserMixin
+from . import db
+import json
+
 
 visits = db.Table('visits',
                   db.Column('visitor_id', db.Integer, db.ForeignKey('visitors.visitor_id')),
@@ -7,7 +12,7 @@ visits = db.Table('visits',
                   )
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     user_id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(30), index=True)
@@ -20,12 +25,29 @@ class User(db.Model):
     def password(self):
         raise AttributeError('password is not a readable attribute')
 
+    def generate_auth_token(self, expiration):
+        s = Serializer(current_app.config['SECRET_KEY'],  expires_in=expiration)
+        return s.dumps({'user_id': self.user_id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return User.query.get(data['user_id'])
+
     @password.setter
     def password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
     def __repr__(self):
         return '<User %r>' % self.first_name + " " + self.last_name
@@ -40,6 +62,10 @@ class Url(db.Model):
     deleted = db.Column(db.Integer, default=0)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
 
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
     def __repr__(self):
         return '<Url %r>' % self.short_url
 
@@ -52,6 +78,10 @@ class Visitor(db.Model):
     urls = db.relationship('Url', secondary=visits,
                            backref=db.backref('visitors', lazy='dynamic'),
                            lazy='dynamic')
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
     def __repr__(self):
         return '<Visitor %r>' % self.name
